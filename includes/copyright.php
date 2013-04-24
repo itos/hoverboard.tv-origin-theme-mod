92289bc62ef35bd753c90654648426740e906d07
Carlos Oporto <carlosoporto@gmail.com>
2013-04-24 14:02:06 -0500

    "add footer copyright"

--- includes/copyright.php
+++ includes/copyright.php
@@ -1,3 +1,3 @@
 <footer id="main-footer">
-	<p id="copyright"><?php printf( __( 'Designed by %1$s | Powered by %2$s', 'Origin' ), '<a href="http://www.elegantthemes.com" title="Premium WordPress Themes">Elegant Themes</a>', '<a href="http://www.wordpress.org">WordPress</a>' ); ?></p>
+	<p id="copyright"><?php printf( 'Hoverboard @ 2013' ); ?></p>
 </footer> <!-- #main-footer -->
